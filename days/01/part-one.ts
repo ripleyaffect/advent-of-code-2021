7278ed6c213a129a262e6f65afe107dc3d3b9d9d
ripleyaffect <t.ripley.peterson@gmail.com>
2021-11-30 22:40:46 -0700

    Day 01

--- days/01/part-one.ts
+++ days/01/part-one.ts
@@ -1,7 +1,17 @@
-import { sum } from '~/lib'
-
 import INPUT from './input'
 
-const partOne = (input) => sum(...input.map(Number))
+export const getIncreasingCount = (readings) => readings.reduce(
+  ({ acc, prev }, curr) => ({
+    // Add one if current is larger than the previous
+    acc: acc + (prev && curr > prev ? 1 : 0),
+    // Set the previous to current
+    prev: curr,
+  }),
+  {
+    acc: 0,
+    prev: null,
+  }).acc
+
+const partOne = (input: string[]) => getIncreasingCount(input.map(Number))
 
 export default () => partOne(INPUT)
--- days/01/part-two.ts
+++ days/01/part-two.ts
@@ -1,5 +1,32 @@
-import INPUT from './input'
+import { sum } from '~/lib'
 
-const partTwo = (input: string[]) => null
+import INPUT from './input'
+import { getIncreasingCount } from './part-one'
+
+const getWindowSums = (readings) => readings.reduce(
+  ({ index, sums, window }, curr) => {
+    // Get the current window
+    // Until the fourth entry, don't slice entries out
+    window = index < 3 ? window.concat(curr) : window.slice(1).concat(curr)
+
+    return {
+      // Pass next its own index
+      index: index + 1,
+      // Add current window sum to the list if window is full
+      sums: index < 2 ? sums : sums.concat(sum(...window)),
+      // Pass next the current window
+      window,
+    }
+  },
+  {
+    index: 0,
+    sums: [],
+    window: [],
+  }
+).sums // We just want the sums
+
+const partTwo = (input: string[]) => getIncreasingCount(
+  getWindowSums(input.map(Number))
+)
 
 export default () => partTwo(INPUT)
