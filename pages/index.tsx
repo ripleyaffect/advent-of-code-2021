5459b85e0d1899ffb2ead32d85a34646ae002fba
ripleyaffect <t.ripley.peterson@gmail.com>
2021-11-30 20:28:42 -0700

    typescript all the things

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Advent of Code - 2021</title>
         <meta name="description" content="Advent of Code - 2021" />
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🎅</text></svg>" />
       </Head>
